d5b885dbf215f94d844da128a0cabf6a7a08b9e4
WilliamAdjei <148203388+WilliamAdjei@users.noreply.github.com>
2023-12-12 11:54:47 +0000

    Creating a Cart.js file, and adding components to the homepage

---
+++ finalproject/src/Homepage.js
@@ -0,0 +1,25 @@
+
+function Welcomepage(){
+
+    return(
+        <div className="StoreName">
+            <div className="Cart">
+                <div className="Home">
+                    <div className="products-recommeded">
+                        <div className="FAQ">
+                            <div className="About Us">
+                                
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
